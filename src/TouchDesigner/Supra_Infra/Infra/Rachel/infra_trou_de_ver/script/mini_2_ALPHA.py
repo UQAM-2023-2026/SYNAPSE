14e5891b421715f0dbe87152526353c4eb985953
rachelp0 <rach01pel@gmail.com>
2025-12-09 14:02:03 -0500

    integ_visuel_alpha

---
+++ src/TouchDesigner/Supra_Infra/Infra/Rachel/infra_trou_de_ver/script/mini_2_ALPHA.py
@@ -0,0 +1,43 @@
+﻿# me - this DAT
+# 
+# channel - the Channel object which has changed
+# sampleIndex - the index of the changed sample
+# val - the numeric value of the changed sample
+# prev - the previous sample value
+# 
+# Make sure the corresponding toggle is enabled in the CHOP Execute DAT.
+
+def onOffToOn(channel, sampleIndex, val, prev):
+	return
+
+def whileOn(channel, sampleIndex, val, prev):
+	return
+
+def onOnToOff(channel, sampleIndex, val, prev):
+	return
+
+def whileOff(channel, sampleIndex, val, prev):
+	return
+
+def onValueChange(channel, sampleIndex, val, prev):
+    if val == 1 and prev == 0:
+        targets = op('targets')  # ton Constant CHOP
+        if channel.name == 'kx':
+            targets.par.value0 = 0.08
+            targets.par.value1 = 1
+        elif channel.name == 'kz':
+            targets.par.value0 = 1
+            targets.par.value1 = 1
+            
+    elif channel.name == 'RZ1_Connected' or 'RZ2_Connected':
+        targets = op('targets')  # ton Constant CHOP
+
+        if (op('Etat_Infra_out')[N1_RZID] == 0 & op('Etat_Infra_out')[N1_Level] == 0) or (op('Etat_Infra_out')[N2_RZID] == 0 & op('Etat_Infra_out')[N2_Level] == 0): # rhizome branché 
+            targets.par.value0 = 0.08
+            targets.par.value1 = 1
+
+        if (op('Etat_Infra_out')[N1_RZID] == 1 & op('Etat_Infra_out')[N1_Level] == 0) or (op('Etat_Infra_out')[N2_RZID] == 1 & op('Etat_Infra_out')[N2_Level] == 0): # rhizome branché
+            targets.par.value0 = 1
+            targets.par.value1 = 1
+    return
+	
