5b7c151a9a67fc3b7be25073e000193382bc2ce9
Clément Boucher <70407746+Jacky202@users.noreply.github.com>
2026-01-19 21:06:20 -0500

    debut animation led

---
+++ src/Arduino/old/StripsAnimation.h
@@ -0,0 +1,9 @@
+#ifndef STRIPS_ANIMATION_H
+#define STRIPS_ANIMATION_H
+
+#include <RhizomeStateAndID.h>
+
+void SetupStrips(RhizomeStateAndID &rh, uint8_t brightness);
+void StripLoop();            // fixed signature to match implementation
+
+#endif
