0ea1fe0749049635738ec9fd88fa9b2792c9f17a
Sasha Bédard <ratoon.1553@gmail.com>
2026-02-11 21:21:49 -0500

    Update laurascenoV1.ino

--- src/Arduino/Scenographie/laurascenoV1/laurascenoV1.ino
+++ src/Arduino/Scenographie/laurascenoV1/laurascenoV1.ino
@@ -5,6 +5,10 @@ const int RPWM = 5; // Marche Avant
 const int LPWM = 6; // Marche Arrière
 const int R_EN = 7; // Enable Droite
 const int L_EN = 8; // Enable Gauche
+const int RPWMA2 = 9; // Marche Avant
+const int LPWMA2 = 10; // Marche Arrière
+const int R_ENA2 = 11; // Enable Droite
+const int L_ENA2 = 12; // Enable Gauche
  
 void setup() {
   Serial.begin(9600); // Pour le débogage sur l'écran PC
@@ -13,15 +17,25 @@ void setup() {
   pinMode(LPWM, OUTPUT);
   pinMode(R_EN, OUTPUT);
   pinMode(L_EN, OUTPUT);
+  pinMode(RPWMA2, OUTPUT);
+  pinMode(LPWMA2, OUTPUT);
+  pinMode(R_ENA2, OUTPUT);
+  pinMode(L_ENA2, OUTPUT);
  
   // ÉTAPE CRUCIALE : On active le module (Enable)
   // Sans ça, le moteur ne recevra jamais de courant
   digitalWrite(R_EN, HIGH);
   digitalWrite(L_EN, HIGH);
+
+  digitalWrite(R_ENA2, HIGH);
+  digitalWrite(L_ENA2, HIGH);
  
   // On s'assure que tout est à l'arrêt au début
   digitalWrite(RPWM, LOW);
   digitalWrite(LPWM, LOW);
+
+  digitalWrite(RPWMA2, LOW);
+  digitalWrite(LPWMA2, LOW);
   
   Serial.println("Systeme pret - Demarrage dans 2 secondes");
   delay(2000);
@@ -32,23 +46,31 @@ void loop() {
   Serial.println("Mouvement: SORTIR >>>");
   digitalWrite(LPWM, LOW);  // On coupe l'arrière
   digitalWrite(RPWM, HIGH); // On active l'avant (Pleine puissance)
+  digitalWrite(LPWMA2, LOW);  // On coupe l'arrière
+  digitalWrite(RPWMA2, HIGH);
   delay(5000);
  
   // --- STOP ---
   Serial.println("--- PAUSE ---");
   digitalWrite(RPWM, LOW);
   digitalWrite(LPWM, LOW);
+  digitalWrite(RPWMA2, LOW);
+  digitalWrite(LPWMA2, LOW);
   delay(1000);
  
   // --- SENS 2 ---
   Serial.println("Mouvement: RENTRER <<<");
   digitalWrite(RPWM, LOW);  // On coupe l'avant
   digitalWrite(LPWM, HIGH); // On active l'arrière (Pleine puissance)
+  digitalWrite(RPWMA2, LOW);  // On coupe l'avant
+  digitalWrite(LPWMA2, HIGH);
   delay(1400);
  
   // --- STOP ---
   Serial.println("--- PAUSE ---");
   digitalWrite(RPWM, LOW);
   digitalWrite(LPWM, LOW);
+  digitalWrite(RPWMA2, LOW);
+  digitalWrite(LPWMA2, LOW);
   delay(5000);
   }
